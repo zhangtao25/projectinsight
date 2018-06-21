8c3183cc9da6dfa000217e1fd4df1a8011c90a37
zhangtao <zhangtao25@lenovo.com>
2018-06-22 02:12:50 +0800

    2018/6/22

---
+++ src/common/plate.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="plate">
+    <div class="SerialNumber" style="font-size: 60px">{{plateDatas.SerialNumber}}</div>
+    <p class="title" style="font-size: 40px">{{plateDatas.title}}</p>
+    <ul style="list-style: disc">
+      <li v-for="(content,index) of plateDatas.contents" :key="index" style="font-size: 22px">{{content}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        // plateDatas:{
+        //   SerialNumber:1,
+        //   title:'APARTMENTS',
+        //   contents:[
+        //     'Location'
+        //   ]
+        // }
+      }
+    },
+    props:['plateDatas']
+  }
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="home" class="margin">
+    <plate :plate-datas="plateDatas1"></plate>
+    <plate :plate-datas="plateDatas2"></plate>
+    <plate :plate-datas="plateDatas3"></plate>
+    <plate :plate-datas="plateDatas4"></plate>
+  </div>
+</template>
+
+<script>
+  import plate from './../common/plate'
+export default {
+  name: 'HelloWorld',
+  components:{
+    plate
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      plateDatas1:{
+        SerialNumber:1,
+        title:'APARTMENTS',
+        contents:[
+          'Location'
+        ]
+      },
+      plateDatas2:{
+        SerialNumber:2,
+        title:'FINANCE',
+        contents:[
+          'Alipay',
+          'Adding a debit card on we chat'
+        ]
+      },
+      plateDatas3:{
+        SerialNumber:3,
+        title:'TRANSPORTATION&COMMUNICATION',
+        contents:[
+          'Phone number&data package',
+          'Metro pass,metro app',
+          'VPN membership',
+          'Renting shared bikes'
+        ]
+      },
+      plateDatas4:{
+        SerialNumber:4,
+        title:'ENTERTAINMENT',
+        contents:[
+          'Foodie Map',
+          'Apps U need'
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
